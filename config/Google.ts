106bcd332f63e93a8e6373853faa8cbd91b20940
Wenhao Leo Liang <36321327+foxhatleo@users.noreply.github.com>
2021-02-24 05:01:20 +0800

    Remove drive.install scope

--- config/Google.ts
+++ config/Google.ts
@@ -5,7 +5,7 @@ export const SCOPES = [
     "https://www.googleapis.com/auth/userinfo.email",
     "https://www.googleapis.com/auth/userinfo.profile",
     // "https://www.googleapis.com/auth/drive",
-    "https://www.googleapis.com/auth/drive.install",
+    // "https://www.googleapis.com/auth/drive.install",
     "https://www.googleapis.com/auth/drive.appdata",
     // "https://www.googleapis.com/auth/drive.metadata",
     "https://www.googleapis.com/auth/drive.file",
