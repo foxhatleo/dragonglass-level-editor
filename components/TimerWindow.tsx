763831c68b2722fa4487b17aed651cd6d1e0e958
Wenhao Leo Liang <36321327+foxhatleo@users.noreply.github.com>
2021-03-31 02:16:37 +0800

    Fix negative sign bug in timer window

--- components/TimerWindow.tsx
+++ components/TimerWindow.tsx
@@ -4,32 +4,56 @@ import {connect, ConnectedProps} from "react-redux";
 import State, {TimerData} from "../redux/store/State";
 import {bindActionCreators} from "redux";
 import * as Dispatcher from "../redux/action/Dispatcher";
-import {CompactPicker} from "react-color";
 import {GLOBAL_TIMER} from "../config/GlobalTimer";
 
 const connector = connect(
     (s: State) => ({timer: s.level.timer || {}}),
     (d) => bindActionCreators(Dispatcher, d),
 );
-export const NO_TIMER_DATA: Required<TimerData> = {
-    levelTime: -1,
-    canvasBaseTime: -1,
-    canvasPerColorTime: -1
+
+type EditTimerData = {
+    levelTime?: string;
+    canvasBaseTime?: string;
+    canvasPerColorTime?: string;
 };
 
+export const NO_TIMER_DATA: Required<EditTimerData> = {
+    levelTime: "-1",
+    canvasBaseTime: "-1",
+    canvasPerColorTime: "-1"
+};
+
+function toEdit(t: TimerData): EditTimerData {
+    if (!t) return {};
+    const a: EditTimerData = {};
+    if (typeof t.levelTime === "number")
+        a.levelTime = t.levelTime.toString();
+    if (typeof t.canvasBaseTime === "number")
+        a.canvasBaseTime = t.canvasBaseTime.toString();
+    if (typeof t.canvasPerColorTime === "number")
+        a.canvasPerColorTime = t.canvasPerColorTime.toString();
+    return a;
+}
+
+const pi = (n: string) => {
+    const z = parseInt(n);
+    if (isNaN(z)) return -1;
+    return z;
+}
+
 const TimerWindow: React.FunctionComponent<ConnectedProps<typeof connector> & { onClose: () => void; show: boolean; }> = (p) => {
-    const [timer, setTimer] = useState<Required<TimerData>>(NO_TIMER_DATA);
+    const [timer, setTimer] = useState<Required<EditTimerData>>(NO_TIMER_DATA);
     useEffect(() => {
-        setTimer({...NO_TIMER_DATA, ...p.timer});
+        setTimer({...NO_TIMER_DATA, ...toEdit(p.timer)});
     }, [p.show]);
     const ok = () => {
-        p.setLevelTime(timer.levelTime);
-        p.setCanvasBaseTime(timer.canvasBaseTime);
-        p.setCanvasPerColorTime(timer.canvasPerColorTime);
+        p.setLevelTime(pi(timer.levelTime));
+        p.setCanvasBaseTime(pi(timer.canvasBaseTime));
+        p.setCanvasPerColorTime(pi(timer.canvasPerColorTime));
         p.onClose();
     };
     const setAttr = (attrName: keyof TimerData, e: ChangeEvent<HTMLInputElement>) => {
-        const v = parseInt(e.currentTarget.value);
+        const v = e.currentTarget.value as string;
         setTimer({...timer, [attrName]: v});
     }
     const cancel = p.onClose;
